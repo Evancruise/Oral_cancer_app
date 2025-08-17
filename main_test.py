1af56d787840eba5541a59b98b90b4a68e72287b
Evan <evan1632885@gmail.com>
2025-08-18 00:22:51 +0800

    [main] Add training/val/test script

---
+++ main_test.py
@@ -0,0 +1,33 @@
+from model_archive.main_entry import model_trainvaltest_process
+
+if __name__ == "__main__":
+
+    '''
+    optimizer_type = session.get("optimizer_type", "adam")
+    lr = session.get("lr", 1e-4)
+    scheduler_mode = session.get("scheduler_mode", "cosineanneal")
+    epochs = int(session.get("total_epochs", 10))
+    ml = session.get("ml", "dinov2")
+    model_tuning_enable = session.get("model_tuning_enable", False)
+    log_enable = session.get("tensorboard_enable", False)
+    start_epoch = int(session.get("start_epoch", 0))
+    input_inference_path = sorted(glob.glob(f"{UPLOAD_DIR}/{patient_id}/*.png"))
+    save_dir = f"{RESULT_DIR}/{patient_id}"
+    '''
+
+    model_trainvaltest_process(
+        optimizer_type="adam",
+        lr=1e-4,
+        scheduler_mode="cosineanneal",
+        epochs=10,
+        mode="train",
+        ml="cascade_resnet",
+        model_tuning_enable=False,
+        log_enable=False,
+        start_epoch=1,
+        input_inference_path=None,
+        save_dir=None,
+        progress_path=None,
+        patient_id=None,
+        db_path=None
+    )
